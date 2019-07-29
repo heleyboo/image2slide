cc8d5208fc40d8567329419a44f70e4ac47656ff
linh.dtn <linh.dtn@cloud9-solutions.com>
2019-07-29 14:34:10 +0700

    Layout

---
+++ image2slide/resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,24 @@
+            <!-- NAVBAR
+            ============================== -->
+            <nav class="navbar navbar-default navbar-fixed-top js-navbar-top js-toggleClass">
+                <div class="container">
+                   <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                      <a class="navbar-brand" href="#home">
+                         <h2>Image2Slide</h2>
+                      </a>
+                   </div>
+                </div>
+             </nav>
+             <!-- INTRO PARALLAX BANNER
+                =================================== -->
+             <div class="parallax parallax_banner2 intro fl_scrn" id="home" data-stellar-background-ratio="0.1">
+                <div class="intro-body">
+                   <div class="container">
+                      <div class="row">
+                         <div class="col-md-6">
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
